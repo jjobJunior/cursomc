8248e2b54f65ba435e6095aaf8699b14830bf2a1
jjobJunior <jobp.s.junior@gmail.com>
2022-01-15 09:22:00 -0300

    Delete de categoria, com tratamento de exceção de relacionamentos!

--- src/main/java/com/jobJunior/cursomc/controllers/exceptions/ControllerExceptionHandler.java
+++ src/main/java/com/jobJunior/cursomc/controllers/exceptions/ControllerExceptionHandler.java
@@ -7,16 +7,26 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
+import com.jobJunior.cursomc.service.exceptions.DataIntegratyViolationException;
 import com.jobJunior.cursomc.service.exceptions.ObjectNotFoundException;
 
 @ControllerAdvice
 public class ControllerExceptionHandler {
 
 	@ExceptionHandler(ObjectNotFoundException.class)
-	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e, HttpServletRequest request) {
+	public ResponseEntity<StandardError> objectNotFoundException(ObjectNotFoundException e,
+			HttpServletRequest request) {
 		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.NOT_FOUND.value(),
 				e.getMessage());
 
 		return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error);
+	}
+
+	@ExceptionHandler(DataIntegratyViolationException.class)
+	public ResponseEntity<StandardError> dataIntegratyViolationException(DataIntegratyViolationException e) {
+		StandardError error = new StandardError(System.currentTimeMillis(), HttpStatus.BAD_REQUEST.value(),
+				e.getMessage());
+
+		return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(error);
 	}
 }
--- src/main/java/com/jobJunior/cursomc/service/CategoriaService.java
+++ src/main/java/com/jobJunior/cursomc/service/CategoriaService.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Service;
 
 import com.jobJunior.cursomc.modelo.Categoria;
 import com.jobJunior.cursomc.repositories.CategoriaRepository;
+import com.jobJunior.cursomc.service.exceptions.DataIntegratyViolationException;
 import com.jobJunior.cursomc.service.exceptions.ObjectNotFoundException;
 
 @Service
@@ -17,8 +18,8 @@ public class CategoriaService {
 
 	public Categoria findById(Integer id) {
 		Optional<Categoria> obj = categoriaRepository.findById(id);
-		return obj.orElseThrow(() -> new ObjectNotFoundException("O Objeto não foi encontrado! Id: "
-		+ id + ", Tipo: " + Categoria.class.getName()));
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"O Objeto não foi encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 	public Categoria insert(Categoria obj) {
@@ -29,6 +30,14 @@ public class CategoriaService {
 	public Categoria update(Categoria obj) {
 		findById(obj.getId());
 		return categoriaRepository.save(obj);
+	}
+
+	public void delete(Integer id) {
+		Categoria categoria = findById(id);
+		if (categoria.getProdutos().size() != 0) {
+			throw new DataIntegratyViolationException("A categoria não pode ser excluida pois esta associada a produtos!");
+		}
+		categoriaRepository.deleteById(id);
 	}
 
 }
