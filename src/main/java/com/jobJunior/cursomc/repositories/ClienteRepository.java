c6b6337f71a36fb317e1c27d589ebd0e05c8cafe
jjobJunior <jobp.s.junior@gmail.com>
2022-02-05 07:29:42 -0300

    Adicionando validação customizada barrando email repetido

--- src/main/java/com/jobJunior/cursomc/repositories/ClienteRepository.java
+++ src/main/java/com/jobJunior/cursomc/repositories/ClienteRepository.java
@@ -2,10 +2,14 @@ package com.jobJunior.cursomc.repositories;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.jobJunior.cursomc.model.Cliente;
 
 @Repository
 public interface ClienteRepository extends JpaRepository<Cliente, Integer> {
 
+	@Transactional(readOnly = true)
+	Cliente findByEmail(String email);
+	
 }
--- src/main/java/com/jobJunior/cursomc/service/validation/ClienteInsertValidator.java
+++ src/main/java/com/jobJunior/cursomc/service/validation/ClienteInsertValidator.java
@@ -6,13 +6,20 @@ import java.util.List;
 import javax.validation.ConstraintValidator;
 import javax.validation.ConstraintValidatorContext;
 
+import org.springframework.beans.factory.annotation.Autowired;
+
 import com.jobJunior.cursomc.controllers.exceptions.FieldMessage;
 import com.jobJunior.cursomc.dto.ClienteNewDTO;
+import com.jobJunior.cursomc.model.Cliente;
 import com.jobJunior.cursomc.model.enuns.TipoCliente;
+import com.jobJunior.cursomc.repositories.ClienteRepository;
 import com.jobJunior.cursomc.service.validation.utils.BR;
 
-
 public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert, ClienteNewDTO> {
+
+	@Autowired
+	private ClienteRepository clienteRepository;
+
 	@Override
 	public void initialize(ClienteInsert ann) {
 	}
@@ -20,13 +27,18 @@ public class ClienteInsertValidator implements ConstraintValidator<ClienteInsert
 	@Override
 	public boolean isValid(ClienteNewDTO objDto, ConstraintValidatorContext context) {
 		List<FieldMessage> list = new ArrayList<>();
-		
+
 		if (objDto.getTipo().equals(TipoCliente.PESSOAFISICA.getCod()) && !BR.isValidCPF(objDto.getcpf_ou_cnpj())) {
 			list.add(new FieldMessage("cpf_ou_cnpj", "CPF inválido!"));
 		}
-		
+
 		if (objDto.getTipo().equals(TipoCliente.PESSOAJURIDICA.getCod()) && !BR.isValidCNPJ(objDto.getcpf_ou_cnpj())) {
 			list.add(new FieldMessage("cpf_ou_cnpj", "CNPJ inválido!"));
+		}
+
+		Cliente aux = clienteRepository.findByEmail(objDto.getEmail());
+		if (aux != null) {
+			list.add(new FieldMessage("email","O email ja esta cadastrado no sistema!"));			
 		}
 
 		for (FieldMessage e : list) {
