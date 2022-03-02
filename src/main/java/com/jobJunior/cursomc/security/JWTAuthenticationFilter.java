06e0d845f8cdc6b3688830de329e1e028f4b917a
jjobJunior <jobp.s.junior@gmail.com>
2022-03-01 22:08:11 -0300

    Expondo o header Authorization

--- src/main/java/com/jobJunior/cursomc/security/JWTAuthenticationFilter.java
+++ src/main/java/com/jobJunior/cursomc/security/JWTAuthenticationFilter.java
@@ -55,6 +55,7 @@ public class JWTAuthenticationFilter extends UsernamePasswordAuthenticationFilte
 		String username = ((UserSpringSecurity) authentication.getPrincipal()).getUsername();
         String token = jwtUtil.generateToken(username);
         response.addHeader("Authorization", "Bearer " + token);
+        response.addHeader("access-control-expose-headers", "Authorization");
 	}
 	
 	//
